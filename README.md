3aa7017e8c609f14649f0a5c49a85d45420bb886
Ignacio Oyarzabal <ignaoya93@gmail.com>
2020-11-02 08:44:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Untitle New Game
+
+Starting out a new game to keep learning c programming with Raylib
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "raylib.h"
+
+
+int main(void)
+{
+	printf("Hello World!");
+	return 0;
+}
