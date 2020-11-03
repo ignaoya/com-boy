3a6e6e5ae098fb4aaf7a22359ebfc1e465cdf2f6
Ignacio Oyarzabal <ignaoya93@gmail.com>
2020-11-03 08:35:44 +0100

    Added very basic circular motion to simulate orbits, orbits are not very stable though, need to work on that

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@ int main(void)
 
 	Entity sun = CreatePlanet((Vector2){ 400.0f, 300.0f }, sunTexture);
 	Entity earth = CreatePlanet((Vector2){ 400.0f, 500.0f }, earthTexture);
+	Entity moon = CreatePlanet((Vector2){ 400.0f, 450.0f }, earthTexture);
+	earth.satellite = &moon;
 
 	bool intro = true;
 	SetTargetFPS(60);
@@ -43,6 +45,9 @@ int main(void)
 		{
 			// Update
 			// -------------------------------------------------------------------------------------
+			float deltaTime = GetFrameTime();
+			UpdatePlanet(&earth, sun.position, deltaTime);
+			UpdatePlanet(&moon, earth.position, deltaTime);
 			
 			// Drawing
 			// -------------------------------------------------------------------------------------
@@ -51,6 +56,7 @@ int main(void)
 				DrawText("Press ESCAPE to Crash", 150, 100, 40, BLACK);
 
 				DrawTextureRec(earth.sprite.texture, earth.sprite.frameRec, earth.position, WHITE);
+				DrawTextureRec(moon.sprite.texture, moon.sprite.frameRec, moon.position, GRAY);
 				DrawTextureRec(sun.sprite.texture, sun.sprite.frameRec, sun.position, WHITE);
 
 			EndDrawing();
@@ -74,8 +80,35 @@ Entity CreatePlanet(Vector2 position, Texture2D texture)
     planet.sprite.framesCounter = 0;
     planet.sprite.framesSpeed = 8;
     planet.speed = 250.0f;
+	planet.angle = 0.0f;
     planet.permanent = 1;
     planet.type = 'p';
+	planet.satellite = NULL;
 
     return planet;
 }
+
+void UpdatePlanet(Entity *planet, Vector2 orbitalCenter, float delta)
+{
+	Vector2 direction = GetOrbitDirection(planet->position, orbitalCenter);
+	planet->position.x -= direction.x * planet->speed * delta;
+	planet->position.y -= direction.y * planet->speed * delta;
+
+	if (planet->satellite != NULL)
+	{
+		planet->satellite->position.x -= direction.x * planet->speed * delta;
+		planet->satellite->position.y -= direction.y * planet->speed * delta;
+	}
+}
+
+Vector2 GetOrbitDirection(Vector2 position, Vector2 orbitalCenter)
+{
+	float distance = Vector2Distance(position, orbitalCenter);
+	Vector2 direction = (Vector2){ orbitalCenter.x - position.x, orbitalCenter.y - position.y };
+	Vector2 nDirection = (Vector2){ direction.x / distance, direction.y / distance };
+	Vector2 orbitDirection = Vector2Rotate(nDirection, 90.2f);
+
+	return orbitDirection;
+}
+
+
--- src/main.h
+++ src/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "stdlib.h"
 #include "raylib.h"
+#include "raymath.h"
 
 // Struct definitions
 // --------------------------------------------------------------------------------------
+
+typedef struct Sprite Sprite;
+typedef struct Entity Entity;
+
 typedef struct Sprite {
     Texture2D texture;
     Rectangle frameRec;
@@ -17,15 +23,18 @@ typedef struct Entity {
     Vector2 position;
     Sprite sprite;
     float speed;
+    float angle;
     int permanent;  // bool to indicate if sprite should be permanent or disappear after anim cycle
     char type;      // char type to indicate which type of Entity it is, and especially which type of ball
+    Entity *satellite;
 } Entity;
 
 
 // Planet Functions
 // --------------------------------------------------------------------------------------
 Entity CreatePlanet(Vector2 position, Texture2D texture);
-void UpdatePlanet(Entity *planet, float delta);
+void UpdatePlanet(Entity *planet, Vector2 orbitalCenter, float delta);
+Vector2 GetOrbitDirection(Vector2 position, Vector2 orbitalCenter);
 
 
 
